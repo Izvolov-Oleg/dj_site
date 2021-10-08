e29bdee715817c2535575aaad742f542604bdfea
Oleg Izvolov <oleg-izvolov@yandex.ru>
2021-10-08 19:02:59 +0300

    first commit

---
+++ women/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse('Страницы приложения women')
+
+def categories(request, catid):
+    return HttpResponse(f'<h1>Статьи по катеогриям</h1><p>{catid}</p>')
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
