f0dfd43a0d5f044440c647824883ab91bd635d30
Oleg Izvolov <oleg-izvolov@yandex.ru>
2021-10-10 01:01:13 +0300

    L8: add static files, modified templates, modified urls.py, views.py, models.py etc

--- women/views.py
+++ women/views.py
@@ -1,20 +1,37 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
+from .models import *
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
 
 def index(request):
-    return HttpResponse('Страницы приложения women.')
+    posts = Women.objects.all()
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'women/index.html', context=context)
 
-def categories(request, catid):
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f'<h1>Статьи по катеогриям</h1><p>{catid}</p>')
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
-def archive(request, year):
-    if int(year) > 2021:
-        return redirect('home', permanent=True)
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+def addpage(request):
+    return HttpResponse('Добавление статьи')
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+def login(request):
+    return HttpResponse('Авторизация')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
